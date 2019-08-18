f83675c47e5432fa9decb086039b76908eb6b6d4
Lucy Ogden-Doyle <lucyod10@gmail.com>
2019-08-18 19:43:55 +1000

    Updates

---
+++ precache-manifest.ec4e9c558552209a4f8449cbda734eb7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17dfcaee52a3ffd845a303dec437a537",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "f1a74f20443568504bcb",
+    "url": "/news-app/static/css/main.93a083dd.chunk.css"
+  },
+  {
+    "revision": "1a87031601ed08f25519",
+    "url": "/news-app/static/js/2.34351e3f.chunk.js"
+  },
+  {
+    "revision": "f1a74f20443568504bcb",
+    "url": "/news-app/static/js/main.b55cd1a1.chunk.js"
+  },
+  {
+    "revision": "fa4b01bc436166f464fa",
+    "url": "/news-app/static/js/runtime~main.dc36b5e3.js"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "/news-app/static/media/cog.9f07a3df.svg"
+  },
+  {
+    "revision": "f3545c3a051e06a368bc081ad4060703",
+    "url": "/news-app/static/media/comment.f3545c3a.svg"
+  },
+  {
+    "revision": "311b7fc943f6469bbe77fe30e939cfc2",
+    "url": "/news-app/static/media/heart.311b7fc9.svg"
+  },
+  {
+    "revision": "b99e8ee27ac446e7f85fd8bb73d72aae",
+    "url": "/news-app/static/media/plus.b99e8ee2.svg"
+  },
+  {
+    "revision": "02f3d23f0b2affba2206adf8317387cc",
+    "url": "/news-app/static/media/share.02f3d23f.svg"
+  },
+  {
+    "revision": "f004e825dddcfc6a610b96cf1939daf0",
+    "url": "/news-app/static/media/sign-in.f004e825.svg"
+  },
+  {
+    "revision": "144ff03084284c8876293946a229fd8e",
+    "url": "/news-app/static/media/sign-out.144ff030.svg"
+  },
+  {
+    "revision": "89b1718ed9280d2c17100398b02c87f2",
+    "url": "/news-app/static/media/sliders.89b1718e.svg"
+  },
+  {
+    "revision": "b1b03f76273177176434141d199180c5",
+    "url": "/news-app/static/media/sync.b1b03f76.svg"
+  }
+]);
